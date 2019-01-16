d48c9ddd60ecf2c8181d9e355ab151a12bf19f77
Fabre Florian <ffabre@hybird.org>
2019-01-16 15:52:06 +0100

    Fix Django 2.1 compatibility issue with FormSetField.  - Remove dependency to unmaintained django-formsetfield  - Fix FormSetWidget.render method

--- demo/django_docusign_demo/forms.py
+++ demo/django_docusign_demo/forms.py
@@ -5,7 +5,8 @@ from django import forms
 from django.forms.formsets import formset_factory
 from django.utils.translation import ugettext_lazy as _
 from django_docusign import api as django_docusign
-from formsetfield.fields import FormSetField
+
+from .formsetfield.fields import FormSetField
 
 
 class SettingsForm(forms.Form):
